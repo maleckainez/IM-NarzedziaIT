78a69952744dd432d90fe4d2ba7fef973cb28bce
Inez Małecka <maleckainez@gmail.com>
2024-07-02 18:00:40 +0200

    Revert "Dodano dwie flagi"
    This reverts commit 2ab0375e996c7826bea5c1df0bcc4af01bce9f98.

--- LAB4/skrypt.sh
+++ LAB4/skrypt.sh
@@ -2,10 +2,10 @@
 function pomoc() {
     echo "Wpisz Lab4.sh --opcja"
     echo "Dostępne opcje:"
-    echo "  -h lub --help        wyświetlanie tego okna pomocy"
-    echo "  -d lub --date        wyświetlanie aktualnej daty"
-    echo "  -l lub --logs        tworzenie stu plików log.txt"
-    echo "  -l numer lub --logs numer  tworzenie określonej ilości plików log"
+    echo "  --help        wyświetlanie tego okna pomocy"
+    echo "  --date        wyświetlanie aktualnej daty"
+    echo "  --logs        tworzenie stu plików log.txt"
+    echo "  --logs numer  tworzenie określonej ilości plików log"
 }
 
 function pokaz_date() {
@@ -24,13 +24,13 @@ function logi() {
 }
 
 case "$1" in
-    -h|--help)
+    --help)
         pomoc
     ;;
-    -d|--date)
+    --date)
         pokaz_date
     ;;
-    -l|--logs)
+    --logs)
         logi "$2"
     ;;
     *)
