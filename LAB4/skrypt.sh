5b2742aabaebef8593efe2aadd40e1ea8f702c1b
Inez Małecka <maleckainez@gmail.com>
2024-07-02 17:33:29 +0200

    Dodano help

--- LAB4/skrypt.sh
+++ LAB4/skrypt.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
-function date(){
-	aktualnaData=$(date +%d/%m/%Y)
-	echo "Dzisiejsza data to: $aktualnaData"
+function pomoc() {
+    echo "Wpisz Lab4.sh --opcja"
+    echo "Dostępne opcje:"
+    echo "  --help        wyświetlanie tego okna pomocy"
+    echo "  --date        wyświetlanie aktualnej daty"
+    echo "  --logs        tworzenie stu plików log.txt"
+    echo "  --logs numer  tworzenie określonej ilości plików log"
 }
-function logi(){
-	local liczba_plikow=${1:-100}
-	for ((i=1; i<=liczba_plikow; i++));do
-		nazwaPliku="log$i.txt"
-		echo "Nazwa Pliku: $nazwaPliku" > "$nazwaPliku"
-		echo "Utworzone przez: $0" >> "$nazwaPliku"
-		echo "Data utworzenia: $(date +%d/%m/%Y)" >> "$nazwaPliku"
-	done
+
+function pokaz_date() {
+    aktualnaData=$(date +%d/%m/%Y)
+    echo "Dzisiejsza data to: $aktualnaData"
 }
+
+function logi() {
+    local liczba_plikow=${1:-100}
+    for ((i=1; i<=liczba_plikow; i++)); do
+        nazwaPliku="log$i.txt"
+        echo "Nazwa Pliku: $nazwaPliku" > "$nazwaPliku"
+        echo "Utworzone przez: $0" >> "$nazwaPliku"
+        echo "Data utworzenia: $(date +%d/%m/%Y)" >> "$nazwaPliku"
+    done
+}
+
 case "$1" in
-	--date)
-		date
-	;;
-	--logs)
-		logi "$2"
-	*)
-		echo"Wybrano błędną opcję $1"
-	;;
+    --help)
+        pomoc
+    ;;
+    --date)
+        pokaz_date
+    ;;
+    --logs)
+        logi "$2"
+    ;;
+    *)
+        echo "Wybrano błędną opcję $1"
+        pomoc
+    ;;
 esac
